a52f58e1b29af14cedb93af24306ffdf2abbc294
Okyop <pcombes80@gmail.com>
2021-03-11 13:49:30 +0100

    to

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Player;
 use App\Models\Team;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Player::factory()->count(30)->create();
         // Team::factory()->count(10)->create();
         $this->call([
             TeamSeeder::class
