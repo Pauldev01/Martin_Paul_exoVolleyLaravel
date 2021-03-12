39a88672b740d14f27675a69e6cdbeefca01387c
Navezm <navez.martin@gmail.com>
2021-03-12 16:52:59 +0100

    Almost done

--- resources/views/partials/dashboard/jumbotronTeam.blade.php
+++ resources/views/partials/dashboard/jumbotronTeam.blade.php
@@ -3,8 +3,13 @@
     <div class="jumbotron">
         <h1 class="display-4">Teams with members</h1>
         <hr class="my-4">
-        @foreach ($players->where('team_id', '!=', 1)->random(2) as $item)
-            <p class="lead">{{$item->teams->name}}</p>
-        @endforeach
+        {{-- @foreach ($playersWith->random(1) as $player)
+            <p class="lead">{{$player->teams->name}}</p>
+            {{$firstID = $player->team_id}}
+            @foreach ($playersWith->except('team_id') as $item)
+                <p class="lead">{{$item->teams->name}}</p>
+                
+            @endforeach
+        @endforeach --}}
     </div>
 </section>
